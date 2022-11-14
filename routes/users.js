4ea7e2d5b7241a3e4e7a41ca83cb52beb1449d03
ManuelEspora <mespora@udesa.edu.ar>
2022-11-14 20:40:39 -0300

    imagenes

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,20 @@ var express = require('express');
 const userController = require('../controllers/userController');
 var router = express.Router();
 
+let multer = require('multer');
+let path = require('path');
+
+let storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname, '../public/images/usuarios'));
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));            
+    }
+});
+
+let upload = multer({storage: storage});
+
 /* GET users listing. */
 router.get('/', userController.user);
 
